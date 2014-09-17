b618ade077dc80098ae9a9005e13df2b42e93421
Jeffles <jeff.sumner@gmail.com>
2014-09-17 04:22:06 -0500

    Added tests for admin auth

--- eventbook/tests.py
+++ eventbook/tests.py
@@ -1,4 +1,4 @@
-from django.test import TestCase
+from django.test import TestCase, LiveServerTestCase, Client
 from django.utils import timezone
 from eventbook.models import Event
 
@@ -26,24 +26,74 @@ class EventTest(TestCase):
         self.assertEquals(only_event, event)
 
         # Check attributes
-        self.assertEquals(only_event.title, 'My first event')
-        self.assertEquals(only_event.text, 'This is my first game night')
-        self.assertEquals(only_event.attendees, 'Jed and Jimmy')
-        self.assertEquals(only_event.location, "Jeff's house")
-        self.assertEquals(only_event.pub_date.day, event.pub_date.day)
-        self.assertEquals(only_event.pub_date.month, event.pub_date.month)
-        self.assertEquals(only_event.pub_date.year, event.pub_date.year)
-        self.assertEquals(only_event.pub_date.hour, event.pub_date.hour)
-        self.assertEquals(only_event.pub_date.minute, event.pub_date.minute)
-        self.assertEquals(only_event.pub_date.second, event.pub_date.second)
+        self.assertEqual(only_event.title, 'My first event')
+        self.assertEqual(only_event.text, 'This is my first game night')
+        self.assertEqual(only_event.attendees, 'Jed and Jimmy')
+        self.assertEqual(only_event.location, "Jeff's house")
+        self.assertEqual(only_event.pub_date.day, event.pub_date.day)
+        self.assertEqual(only_event.pub_date.month, event.pub_date.month)
+        self.assertEqual(only_event.pub_date.year, event.pub_date.year)
+        self.assertEqual(only_event.pub_date.hour, event.pub_date.hour)
+        self.assertEqual(only_event.pub_date.minute, event.pub_date.minute)
+        self.assertEqual(only_event.pub_date.second, event.pub_date.second)
 
-        self.assertEquals(only_event.start_date.day, event.start_date.day)
-        self.assertEquals(only_event.start_date.month, event.start_date.month)
-        self.assertEquals(only_event.start_date.year, event.start_date.year)
-        self.assertEquals(only_event.start_date.hour, event.start_date.hour)
-        self.assertEquals(only_event.start_date.minute, event.start_date.minute)
-        self.assertEquals(only_event.start_date.second, event.start_date.second)
+        self.assertEqual(only_event.start_date.day, event.start_date.day)
+        self.assertEqual(only_event.start_date.month, event.start_date.month)
+        self.assertEqual(only_event.start_date.year, event.start_date.year)
+        self.assertEqual(only_event.start_date.hour, event.start_date.hour)
+        self.assertEqual(only_event.start_date.minute, event.start_date.minute)
+        self.assertEqual(only_event.start_date.second, event.start_date.second)
 
-        self.assertEquals(only_event.end_time.hour, event.end_time.hour)
-        self.assertEquals(only_event.end_time.minute, event.end_time.minute)
-        self.assertEquals(only_event.end_time.second, event.end_time.second)
+        self.assertEqual(only_event.end_time.hour, event.end_time.hour)
+        self.assertEqual(only_event.end_time.minute, event.end_time.minute)
+        self.assertEqual(only_event.end_time.second, event.end_time.second)
+
+class AdminTest(LiveServerTestCase):
+    fixtures = ['users.json']
+
+    def test_login(self):
+        # Create client
+        c = Client()
+
+        # Get login page
+        response = c.get('/admin/', follow=True)
+
+        # Check response code
+        self.assertEqual(response.status_code, 200)
+
+        # Check 'Log in' in response
+        self.assertTrue('Log in' in response.content)
+
+        # Log the user in
+        c.login(username='bobsmith', password="password")
+
+        # Check response code
+        response = c.get('/admin/', follow=True)
+        self.assertEquals(response.status_code, 200)
+
+        # Check 'Log out' in response
+        self.assertTrue('Log out' in response.content)
+
+    def test_logout(self):
+        # Create client
+        c = Client()
+
+        # Log in
+        c.login(username='bobsmith', password="password")
+
+        # Check response code
+        response = c.get('/admin/', follow=True)
+        self.assertEquals(response.status_code, 200)
+
+        # Check 'Log out' in response
+        self.assertTrue('Log out' in response.content)
+
+        # Log out
+        c.logout()
+
+        # Check response code
+        response = c.get('/admin/', follow=True)
+        self.assertEquals(response.status_code, 200)
+
+        # Check 'Log in' in response
+        self.assertTrue('Log in' in response.content)
