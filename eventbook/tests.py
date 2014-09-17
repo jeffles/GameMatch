0161578e4d186ad1dccc1ec5ce09188446c43300
Jeffles <jeff.sumner@gmail.com>
2014-09-17 06:48:29 -0500

    Post admin tests in place

--- eventbook/tests.py
+++ eventbook/tests.py
@@ -5,10 +5,8 @@ from eventbook.models import Event
 
 class EventTest(TestCase):
     def test_create_event(self):
-        # Create the event
         event = Event()
 
-        # Set the attributes
         event.title = 'My first event'
         event.text = 'This is my first game night'
         event.pub_date = timezone.now()
@@ -16,8 +14,6 @@ class EventTest(TestCase):
         event.end_time = timezone.now().time()
         event.attendees = 'Jed and Jimmy'
         event.location = "Jeff's house"
-
-        # Save it
         event.save()
 
         # Check we can find it
@@ -52,6 +48,7 @@ class EventTest(TestCase):
 
 class AdminTest(LiveServerTestCase):
     fixtures = ['users.json']
+    reset_sequences = True
 
     def setUp(self):
         self.client = Client()
@@ -74,3 +71,109 @@ class AdminTest(LiveServerTestCase):
         response = self.client.get('/admin/', follow=True)
         self.assertEquals(response.status_code, 200)
         self.assertTrue('Log in' in response.content)
+
+    def test_create_event(self):
+        self.client.login(username='bobsmith', password="password")
+        response = self.client.get('/admin/eventbook/event/add/')
+        self.assertEquals(response.status_code, 200)
+
+        response = self.client.post('/admin/eventbook/event/add/', {
+            'title': 'My first event',
+            'text': 'This is my first game night',
+            'pub_date_0': '2013-12-28',
+            'pub_date_1': '22:00:04',
+            'start_date_0': '2013-12-28',
+            'start_date_1': '22:00:04',
+            'end_time': '23:00:04',
+            'attendees': 'Jed and Jimmy',
+            'location': "Jeff's house"
+        },
+        follow=True
+        )
+        self.assertEquals(response.status_code, 200)
+
+        self.assertTrue('added successfully' in response.content)
+        all_events = Event.objects.all()
+        self.assertEquals(len(all_events), 1)
+
+    def test_edit_event(self):
+        event = Event()
+        event.title = 'My first event'
+        event.text = 'This is my first game night'
+        event.pub_date = timezone.now()
+        event.start_date = timezone.now()
+        event.end_time = timezone.now().time()
+        event.attendees = 'Jed and Jimmy'
+        event.location = 'Jeff'
+        event.save()
+
+        # Check event amended
+        all_events = Event.objects.all()
+        self.assertEquals(len(all_events), 1)
+        only_event = all_events[0]
+        self.assertEquals(only_event.title, 'My first event')
+        self.assertEquals(only_event.text, 'This is my first game night')
+
+        self.client.login(username='bobsmith', password="password")
+
+        response = self.client.get('/admin/eventbook/event/')
+        self.assertEquals(response.status_code, 200)
+
+        #TODO figure out why it is getting created as 2 instead of 1
+        response = self.client.get('/admin/eventbook/event/'+str(only_event.pk)+'/')
+        self.assertEquals(response.status_code, 200)
+
+        response = self.client.post('/admin/eventbook/event/'+str(only_event.pk)+'/', {
+            'title': 'My second event',
+            'text': 'This is my second game night',
+            'pub_date_0': '2013-12-28',
+            'pub_date_1': '22:00:04',
+            'start_date_0': '2013-12-28',
+            'start_date_1': '22:00:04',
+            'end_time': '23:00:04',
+            'attendees': 'Jed and Jimmy',
+            'location': "Jeff's house"
+        },
+        follow=True
+        )
+        self.assertEquals(response.status_code, 200)
+
+        self.assertTrue('changed successfully' in response.content)
+
+        # Check event amended
+        all_events = Event.objects.all()
+        self.assertEquals(len(all_events), 1)
+        only_event = all_events[0]
+        self.assertEquals(only_event.title, 'My second event')
+        self.assertEquals(only_event.text, 'This is my second game night')
+
+    def test_delete_event(self):
+        event = Event()
+        event.title = 'My first event'
+        event.text = 'This is my first game night'
+        event.pub_date = timezone.now()
+        event.start_date = timezone.now()
+        event.end_time = timezone.now().time()
+        event.attendees = 'Jed and Jimmy'
+        event.location = 'Jeff'
+        event.save()
+
+        # Check new event saved
+        all_events = Event.objects.all()
+        self.assertEquals(len(all_events), 1)
+
+        # Log in
+        self.client.login(username='bobsmith', password="password")
+
+        # Delete the event
+        response = self.client.post('/admin/eventbook/event/'+str(event.pk)+'/delete/', {
+            'post': 'yes'
+        }, follow=True)
+        self.assertEquals(response.status_code, 200)
+
+        # Check deleted successfully
+        self.assertTrue('deleted successfully' in response.content)
+
+        # Check event amended
+        all_events = Event.objects.all()
+        self.assertEquals(len(all_events), 0)
