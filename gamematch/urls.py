732770e4f7f7c4f5abc1c0f9f6b3e366edd42592
Jeffles <jeff.sumner@gmail.com>
2014-11-02 14:06:53 -0600

    Changes to url for friends

--- gamematch/urls.py
+++ gamematch/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, url
 from django.views.generic import ListView, DetailView
 from eventbook.models import Event
+from eventbook.views import EventCreate, EventUpdate, ShowFriends
 
 urlpatterns = patterns('',
     # Index
@@ -12,9 +13,17 @@ urlpatterns = patterns('',
         ),
 
     # Individual events
+    url(r'^create/?$', EventCreate.as_view(), name='create'),
+    url(r'^showfriends/?', ShowFriends, name='showfriends'),
+
+    # Individual events
     url(r'^(?P<pub_date__year>\d{4})/(?P<pub_date__month>\d{1,2})/(?P<slug>[a-zA-Z0-9-]+)/?$', DetailView.as_view(
         model=Event,
         ),
         name='event'
         ),
+
+    # Individual events
+    url(r'^(?P<pub_date__year>\d{4})/(?P<pub_date__month>\d{1,2})/(?P<slug>[a-zA-Z0-9-]+)/update/?$',
+        EventUpdate.as_view(), name='update'),
 )
